68424017f707da7b4842d6914c19ffdb34adc4ed
muchallen <muchallen@gmail.com>
2021-04-26 11:15:50 +0200

    make it better

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const router = require('./routes/auth')
 const mainApp = require('./routes/mainApp')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
-const functions = require('./functions/firebase-functions');
-var PORT = process.env.PORT || 5000;
+var PORT = process.env.PORT || 3100;
 
 const app = express();
 
@@ -19,14 +18,12 @@ app.set('view engine', 'ejs');
 
 
 app.listen(PORT, ()=>{
-    console.log('listening at port 3100')
+    console.log('listening at port'+ PORT)
 })
 
 app.use(router)
 app.use(mainApp)
 
-
-//exports.app = functions.https.onRequest(app);
 
 app.get('/users', (req, res)=>res.render('users')); 
 
